08df27a79f7aab49558de503086772855ad047bf
Kking <hellhands@yandex.ru>
2019-06-14 11:42:24 +0300

    this is commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,42 @@
 const gulp = require('gulp');
-const del = require('del'); // Delete folders
+const del = require('del');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const plumber = require('gulp-plumber'); // Errors
-const nunjucksRender = require('gulp-nunjucks-render');// HTML
+const csso = require("gulp-csso");
+const plumber = require('gulp-plumber');
+const nunjucksRender = require('gulp-nunjucks-render');
 //const svgSprite = require('gulp-svg-sprite');
 
 const path = {
   build: {
-    html: 'build/',
-    njk: 'build/',
-    css: 'build/assets/css/',
-    js: 'build/assets/js/',
+    html:   'build/',
+    njk:    'build/',
+    css:    'build/assets/css/',
+    js:     'build/assets/js/',
+    images: 'build/images/',
+    img:    'build/assets/img/'
   },
   src: {
-    html: './dev/html/*.html',
-    njk: './dev/njk/*.njk',
-    css: './dev/sass/styles.scss',
-    js: './dev/js/*.js',
+    html:   './dev/html/*.html',
+    njk:    './dev/njk/*.njk',
+    css:    './dev/sass/main.scss',
+    js:     './dev/js/*.js',
+    images: './dev/images/html/**/*.{jpg,jpeg,png,gif,svg}',
+    img:    './dev/images/css/**/*.{jpg,jpeg,png,gif,svg}'
   },
   watch: {
-    html: './dev/html/**/*.html',
-    njk: './dev/njk/**/*.njk',
-    css: './dev/sass/**/*.{scss,sass}',
-    js: './dev/js/*.js',
+    html:   './dev/html/**/*.html',
+    njk:    './dev/njk/**/*.njk',
+    css:    './dev/sass/**/*.{scss,sass}',
+    js:     './dev/js/*.js',
+    images: './dev/images/html/**/*.{jpg,jpeg,png,gif,svg}',
+    img:    './dev/images/css/**/*.{jpg,jpeg,png,gif,svg}'
   }
 };
 
-//HTML nunjucks
+// nunjucks
 function njk() {
   return gulp.src(path.src.njk)
     .pipe(nunjucksRender({
@@ -39,20 +46,40 @@ function njk() {
     .pipe(browserSync.stream());
 }
 
-//styles
+// HTML
+function html() {
+  return gulp.src(path.src.html)
+    .pipe(gulp.dest(path.build.html))
+    .pipe(browserSync.stream());
+}
+
+// IMAGES
+// images for html
+function images() {
+  return gulp.src(path.src.images)
+    .pipe(gulp.dest(path.build.images))
+    .pipe(browserSync.stream());
+}
+
+// images for css
+function img() {
+  return gulp.src(path.src.img)
+    .pipe(gulp.dest(path.build.img))
+    .pipe(browserSync.stream());
+}
+
+// styles
 function styles() {
   return gulp.src(path.src.css)
     .pipe(plumber())
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(postcss([
-      autoprefixer({
-        browsers: ['last 3 version']
-      })
-    ]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
     .pipe(gulp.dest(path.build.css))
     .pipe(browserSync.stream());
 }
 
+// Scripts
 function scripts() {
   return gulp.src(path.src.js)
     .pipe(gulp.dest(path.build.js))
@@ -64,11 +91,8 @@ function watch() {
     server: "build/"
   });
 
-  //Следить за CSS файлами
   gulp.watch(path.watch.css, styles);
-  //Следить за JS файлами
   gulp.watch(path.watch.js, scripts);
-  //При изменении HTML запустить синхронизацию
   gulp.watch(path.watch.njk, njk);
 }
 
@@ -81,7 +105,9 @@ function clean() {
 gulp.task('html', njk);
 gulp.task('css', styles);
 gulp.task('js', scripts);
+gulp.task('img', img);
+gulp.task('images', images);
 gulp.task('del', clean);
 
-gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts,njk)));
+gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts,njk,images,img)));
 gulp.task('dev', gulp.series('build',watch));
